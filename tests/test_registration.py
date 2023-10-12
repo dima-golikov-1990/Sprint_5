c7d340f9d6485456e25dad36005710ea88054e77
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2023-10-12 13:27:39 +0300

    Update test_registration.py
    Сделал импорт класса Locators.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,36 +2,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from datatest import nameForRegistration, emailForRegistrtion, passwordForRegistration, incorrectPasswordForRegistration
-from locators import *
+from locators import Locators
 
 class TestRegistration:
     def test_reginstration_with_valid_password(self, driver):
-        driver.find_element(By.XPATH, loginButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        driver.find_element(By.XPATH, Locators.loginButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         driver.find_element(By.LINK_TEXT, 'Зарегистрироваться').click()
 
-        driver.find_element(By.XPATH, registrationNameField).send_keys(nameForRegistration)
-        driver.find_element(By.XPATH, registrationEmailField).send_keys(emailForRegistrtion)
-        driver.find_element(By.XPATH, registrationPasswordField).send_keys(passwordForRegistration)
+        driver.find_element(By.XPATH, Locators.registrationNameField).send_keys(nameForRegistration)
+        driver.find_element(By.XPATH, Locators.registrationEmailField).send_keys(emailForRegistrtion)
+        driver.find_element(By.XPATH, Locators.registrationPasswordField).send_keys(passwordForRegistration)
 
-        driver.find_element(By.XPATH, registerButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        driver.find_element(By.XPATH, Locators.registerButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         assert "login" in driver.current_url
 
     def test_reginstration_with_invalid_password(self, driver):
-        driver.find_element(By.XPATH, loginButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        driver.find_element(By.XPATH, Locators.loginButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         driver.find_element(By.LINK_TEXT, 'Зарегистрироваться').click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, registrationLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.registrationLink)))
 
-        driver.find_element(By.XPATH, registrationNameField).send_keys(nameForRegistration)
-        driver.find_element(By.XPATH, registrationEmailField).send_keys(emailForRegistrtion)
-        driver.find_element(By.XPATH, registrationPasswordField).send_keys(incorrectPasswordForRegistration)
+        driver.find_element(By.XPATH, Locators.registrationNameField).send_keys(nameForRegistration)
+        driver.find_element(By.XPATH, Locators.registrationEmailField).send_keys(emailForRegistrtion)
+        driver.find_element(By.XPATH, Locators.registrationPasswordField).send_keys(incorrectPasswordForRegistration)
 
-        driver.find_element(By.XPATH, registerButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, incorrectPasswordText)))
+        driver.find_element(By.XPATH, Locators.registerButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.incorrectPasswordText)))
 
-        assert driver.find_element(By.XPATH, incorrectPasswordText).text == "Некорректный пароль"
+        assert driver.find_element(By.XPATH, Locators.incorrectPasswordText).text == "Некорректный пароль"
