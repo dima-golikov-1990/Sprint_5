7e742e43aeca325f0322368474f3f924487f1572
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2023-10-12 13:11:07 +0300

    Update test_login.py
    Импортировал класс Locators.
    Переписал в соответствии с этим тесты.

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,66 +2,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from datatest import emailForLogin, passwordForLogin
-from locators import loginButton, emailField, passwordField, enterButton, profileButton, makeBurgerText, enterLink, registrationLink, recoverPasswordLink
+from locators import Locators
 
 class TestLogin:
     def test_login_using_the_login_to_account_button(self, driver):
-        driver.find_element(By.XPATH, loginButton).click()
+        driver.find_element(By.XPATH, Locators.loginButton).click()
 
-        driver.find_element(By.XPATH, emailField).send_keys(emailForLogin)
-        driver.find_element(By.XPATH, passwordField).send_keys(passwordForLogin)
-        driver.find_element(By.XPATH, enterButton).click()
+        driver.find_element(By.XPATH, Locators.emailField).send_keys(emailForLogin)
+        driver.find_element(By.XPATH, Locators.passwordField).send_keys(passwordForLogin)
+        driver.find_element(By.XPATH, Locators.enterButton).click()
 
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
 
     def test_login_using_the_account_button(self, driver):
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
-        driver.find_element(By.XPATH, profileButton).click()
+        driver.find_element(By.XPATH, Locators.profileButton).click()
 
-        driver.find_element(By.XPATH, emailField).send_keys(emailForLogin)
-        driver.find_element(By.XPATH, passwordField).send_keys(passwordForLogin)
-        driver.find_element(By.XPATH, enterButton).click()
+        driver.find_element(By.XPATH, Locators.emailField).send_keys(emailForLogin)
+        driver.find_element(By.XPATH, Locators.passwordField).send_keys(passwordForLogin)
+        driver.find_element(By.XPATH, Locators.enterButton).click()
 
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
 
     def test_login_in_registration_page(self, driver):
-        driver.find_element(By.XPATH, loginButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        driver.find_element(By.XPATH, Locators.loginButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         driver.find_element(By.LINK_TEXT, 'Зарегистрироваться').click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, registrationLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.registrationLink)))
 
         driver.find_element(By.LINK_TEXT, 'Войти').click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
-        driver.find_element(By.XPATH, emailField).send_keys(emailForLogin)
-        driver.find_element(By.XPATH, passwordField).send_keys(passwordForLogin)
-        driver.find_element(By.XPATH, enterButton).click()
+        driver.find_element(By.XPATH, Locators.emailField).send_keys(emailForLogin)
+        driver.find_element(By.XPATH, Locators.passwordField).send_keys(passwordForLogin)
+        driver.find_element(By.XPATH, Locators.enterButton).click()
 
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
 
 
     def test_login_in_recover_password_page(self, driver):
         driver.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         driver.find_element(By.LINK_TEXT, 'Восстановить пароль').click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, recoverPasswordLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.recoverPasswordLink)))
 
         driver.find_element(By.LINK_TEXT, 'Войти').click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
-        driver.find_element(By.XPATH, emailField).send_keys(emailForLogin)
-        driver.find_element(By.XPATH, passwordField).send_keys(passwordForLogin)
-        driver.find_element(By.XPATH, enterButton).click()
+        driver.find_element(By.XPATH, Locators.emailField).send_keys(emailForLogin)
+        driver.find_element(By.XPATH, Locators.passwordField).send_keys(passwordForLogin)
+        driver.find_element(By.XPATH, Locators.enterButton).click()
 
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
