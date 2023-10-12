df2260ba9f14b96e2e829810cdc28037d4eada57
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2023-10-12 13:26:26 +0300

    Update test_logout.py
    Сделал импорт класса Locators.

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -2,26 +2,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from datatest import emailForLogin, passwordForLogin
-from locators import *
+from locators import Locators
 
 class TestLogout:
     def test_logout_from_profile(self, driver):
-        driver.find_element(By.XPATH, loginButton).click()
+        driver.find_element(By.XPATH, Locators.loginButton).click()
 
-        driver.find_element(By.XPATH, emailField).send_keys(emailForLogin)
-        driver.find_element(By.XPATH, passwordField).send_keys(passwordForLogin)
-        driver.find_element(By.XPATH, enterButton).click()
+        driver.find_element(By.XPATH, Locators.emailField).send_keys(emailForLogin)
+        driver.find_element(By.XPATH, Locators.passwordField).send_keys(passwordForLogin)
+        driver.find_element(By.XPATH, Locators.enterButton).click()
 
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, makeBurgerText)))
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.makeBurgerText)))
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
 
-        driver.find_element(By.XPATH, profileButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, profileLink)))
+        driver.find_element(By.XPATH, Locators.profileButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.profileLink)))
 
         assert "profile" in driver.current_url
 
-        driver.find_element(By.XPATH, logoutButton).click()
-        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, enterLink)))
+        driver.find_element(By.XPATH, Locators.logoutButton).click()
+        WebDriverWait(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, Locators.enterLink)))
 
         assert "login" in driver.current_url
